134c36f865254438cc5f6879166002c1b866a9d1
Elias <eliasmena.2911@gmail.com>
2022-04-13 20:11:34 -0600

    Registration Form

---
+++ Taller1/Taller1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Taller1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Taller1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a08ce134-84fa-4912-8b51-b00b473beb07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Taller1/Taller1/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Taller1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (CheckBoxAccept.Checked)
+            {
+                string password = TxtPassword.Text;
+                if(TxtConfirmPasswor.Text == password)
+                {
+                    string username = TxtUser.Text;
+                    string email = TxtEmail.Text;
+                    string lastName = TxtLastname.Text;
+                    string firstName = TxtFirstName.Text;
+                    string country = DropDownCountry.SelectedItem.Text;
+                    string province = TxtProvincia.Text;
+                    string postcode = TxtPostCode.Text;
+                    string gender = "";
+                    if (RadioBtnMale.Checked)
+                    {
+                        gender = RadioBtnMale.Text;
+                    }
+                    if (RadioBtnMale.Checked)
+                    {
+                        gender = RadioBtnFemale.Text;
+                    }
+                    string birthday = TxtBirthDate.Text;
+                    string comments = TxtComments.Text;
+
+                    
+                }
+                else
+                {
+                   // Console.WriteLine("Contraseña incorrecta");
+                }
+            }
+            else
+            {
+                //Console.WriteLine("Debe aceptar los términos");
+            }
+        }
+
+    }
+}
